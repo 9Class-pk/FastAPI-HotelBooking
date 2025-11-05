350896a96524cd0b99617dce44a174bafba269f1
loren <lorenofq@gmail.com>
2025-11-06 03:24:01 +0600

    add. new_routers

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException, Depends, APIRouter
-
-from app.api.soical_auth import social_router
-from app.db.database import  SessionLocal
-from app.api import (user, country, city, hotel, hotelimage,
-                     favourite, favouriteitem, review, room,
-                     roomimage, service, bookings, health_status,
-                     auth, soical_auth)
-from app.admin.setup import setup_admin
-from starlette.middleware.sessions import SessionMiddleware
-from app.middlewares.middleware import LoggingMiddleware
-import os
+#
+#from app.api.soical_auth import social_router
+#from app.db.database import  SessionLocal
+##from app.api import (user, country, city, hotel, hotelimage,
+#                     favourite, favouriteitem, review, room,
+#                     roomimage, service, bookings, health_status,
+#                     auth, soical_auth)
+#from app.admin.setup import setup_admin
+#from starlette.middleware.sessions import SessionMiddleware
+#from app.middlewares.middleware import LoggingMiddleware
+#import os
+import uvicorn
 from fastapi.responses import HTMLResponse
+from app.routers.router import *
+from app.db.database import  SessionLocal
 
 
 async def get_db():
@@ -22,32 +25,7 @@ async def get_db():
         db.close()
 
 
-booking = FastAPI()
-
-booking.include_router(user.user_router)
-booking.include_router(auth.auth_router)
-booking.include_router(soical_auth.social_router)
-booking.include_router(country.country_router)
-booking.include_router(city.city_router)
-booking.include_router(hotel.hotel_router)
-booking.include_router(hotelimage.hotel_image_router)
-booking.include_router(room.room_router)
-booking.include_router(roomimage.room_image_router)
-booking.include_router(bookings.booking_router)
-booking.include_router(service.service_router)
-booking.include_router(review.review_router)
-booking.include_router(favourite.favourite_router)
-booking.include_router(favouriteitem.favouriteitem_router)
-booking.include_router(health_status.health_routers)
-
-
-#oauth middlewares
-booking.add_middleware(SessionMiddleware, secret_key="SECRET_KEY")
-booking.add_middleware(LoggingMiddleware)
-#admin
-setup_admin(booking)
-
-
+#booking = FastAPI() если в роутере есть booking, то не нужно создать booking
 
 @booking.get("/", response_class=HTMLResponse)
 async def Home():
